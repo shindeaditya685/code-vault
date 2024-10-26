982a596ccf4590bf6730f9adc499cf38219f3783
Aditya Shinde <shindeaditya685@gmail.com>
2024-10-26 12:35:29 +0530

    API base url added

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL:
+    "https://code-vault-4ofeghaaj-shindeaditya685s-projects.vercel.app/api",
 });
 
 export default api;

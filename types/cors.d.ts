c3adbc4c247f1ab863d987b6c91404b6d58918a8
Aditya Shinde <shindeaditya685@gmail.com>
2024-10-26 13:39:39 +0530

    Removed the unneccesary code

--- types/cors.d.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export type MiddlewareFunction = (
-  req: NextApiRequest,
-  res: NextApiResponse,
-  callback: (result: Error | null) => void
-) => void;

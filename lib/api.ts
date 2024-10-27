46320f7907428a6a84a3d4fefe5063ea079ede81
Aditya Shinde <shindeaditya685@gmail.com>
2024-10-27 07:56:46 +0530

    solved the issue of cors but actually it was not a issue.

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "https://code-vault-4ofeghaaj-shindeaditya685s-projects.vercel.app/api",
+  baseURL: "/api",
 });
 
 export default api;
